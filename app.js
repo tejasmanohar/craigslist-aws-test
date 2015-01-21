1780c91e651ad79af93f5c033f19ca735a7abc8f
Tejas Manohar <me@tejasmanohar.com>
2015-01-20 19:41:00 -0600

    3000 for pros

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var request = require('request');
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
   request('http://sfbay.craigslist.org/search/apa?query=cheap', function (error, response, body) {
